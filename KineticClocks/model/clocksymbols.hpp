c24e19b3143b7c004e8cbdcb413f473837545660
Pat <Pgulotta@gmail.com>
2016-09-08 12:31:28 -0500

    Signed-off-by: Pat <Pgulotta@gmail.com>

--- KineticClocks/model/clocksymbols.hpp
+++ KineticClocks/model/clocksymbols.hpp
@@ -30,7 +30,7 @@ class ClockSymbols
 {
 public:
     typedef Symbol::ClocksArray::GridCIterator SymbolClocksCIterator;
-    typedef  std::pair<ClockSymbols::SymbolClocksCIterator ,ClockSymbols::SymbolClocksCIterator>  Citerators;
+    typedef  std::pair<SymbolClocksCIterator ,SymbolClocksCIterator>  Citerators;
     Citerators GetRow(SymbolName name, int row) ;
     static const int RowCount = (int)SymbolName::HorizontalLine +1;
     static SymbolName GetSymbolName(QChar symbol);
